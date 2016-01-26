ccd525a3ca2ad48c11b2505dc6b068d5d1a49644
Sofia Celi <sceli@thoughtworks.com>
2016-01-26 15:26:59 -0500

    Dog Launcher improved

--- Dog.java
+++ Dog.java
@@ -1,10 +1,12 @@
 public class Dog extends Animal {
    
     String trinomialName = "Dog";
-    String size = "big"; 
-    String noise = "Ruff, ruff!";
+    String size = "big";
+    String noise = "Ruff, Ruff!";
     String food = "meat";
     boolean motility = true;
-     
-}
+    boolean hunger = true;
+    boolean sleep = false; 
+         
+    }
      
--- DogLauncher.java
+++ DogLauncher.java
@@ -2,6 +2,9 @@ public class DogLauncher {
     public static void main (String[] args) {
         Dog balto = new Dog();
         System.out.println("The animal class is " + balto.trinomialName);
-        System.out.println(balto.trinomialName + " eats " + balto.eat(true, "meat"));
+        System.out.println(balto.trinomialName +"'s size is " + balto.size);
+        System.out.println(balto.trinomialName + " eats " + balto.eat(balto.hunger, balto.food) + ".");
+        System.out.println(balto.trinomialName + " barks and says: " + balto.noise);
+        System.out.println("Is " +  balto.trinomialName + " sleeping? " + balto.sleep);
 }
 }
